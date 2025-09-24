93ad6407bdbfd547afdac04c2335a0a94188c650
katekyosennajr <katekyosennajr@gmail.com>
2025-09-24 20:13:47 +0700

    Hapus duplicate example.spec.js

--- tests/tests/example.spec.js
+++
@@ -1,8 +0,0 @@
-import { test, expect } from '@playwright/test';
-
-test('Cek halaman utama Playwright', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-  
-  
-  await expect(page).toHaveTitle(/Playwright/);
-});
